6d6aa6846c4fabe0d9e4501457e7542515460d2b
yrung <yrungen@163.com>
2018-07-19 17:25:15 +0800

    Initial commit

---
+++ src/main/java/com/example/myjpa/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.myjpa.controller;
+
+import com.example.myjpa.model.Moan;
+import com.example.myjpa.repository.MoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * Author YRG
+ * Created by on 2018/7/18.
+ * Describe
+ */
+@RestController
+public class UserController {
+
+    @Autowired
+    MoanRepository moanRepository;
+
+    @PostMapping(value = "save/moan")
+    public List<Moan> saveMoan(@Valid Moan moan) {
+
+        moanRepository.save(moan);
+
+        return null;
+    }
+
+
+
+
+}
